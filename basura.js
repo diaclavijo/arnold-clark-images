ed7e7196987dff09bf036d9f30601dcf0521c078
diaclavijo <david@diazclavijo.com>
2014-07-17 21:36:33 +0100

    removed stupide files

--- basura.js
+++
@@ -1,9 +0,0 @@
-
-<![CDATA[<vehicledetails realsale="false" blockimagedomain="blockcache.arnoldclark.com" dmsimagedomain="imagecache.arnoldclark.com" phonetype="used" cartype="3">
-  <vehicle><yearplate>2005 (05)</yearplate><baseplate>05</baseplate><uid>7445</uid>
-    <stock_reference>ARNCM-U-515377</stock_reference>
-
-    <feed_id>ARNCM</feed_id>
-    <full_registration>DK05XCR</full_registration>
-
-  http://imagecache.arnoldclark.com/imageserver/ARRCNXC5M0-KUD5/350/i/
